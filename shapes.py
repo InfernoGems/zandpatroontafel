66f65789b18bd009cf2fc7564f3b059dfda4f00a
Sirezner <33341018+Sirezner@users.noreply.github.com>
2017-11-05 10:14:48 +0100

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -39,7 +39,7 @@ class Curve(Shape):
     def __init__(self, points, step_size = None):
         if step_size is None:
             step_size = 1
-        super().__init__(points)
+        self._points = points
         f = interp1d([x for x, y in points], [y for x, y in points])
         for a in arange(0, max(points, key = lambda xy: xy[0])[0] + step_size, step_size):
             self._points.append((a, f(a)))
