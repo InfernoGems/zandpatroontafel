f1c997ed202896a8e9180b55c934a115595d70f1
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2017-11-15 18:21:47 +0100

    Removed outdated queue actions

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from threading import Thread
 from shutil import move
 import json, os, base64, driver
 
-HOST = '192.168.2.61'
+HOST = '192.168.1.218'
 PORT = 80
 PIN = '0000'
 
@@ -13,102 +13,97 @@ d = driver.DummyDriver(queue)
 
 
 def handle_json(json_data):
-    print(json_data)
-    try:
-        if json_data['pin'] != PIN:
-            return {'status': 'failed', 'message': 'Incorrect pin. '}
+	print(json_data)
+	try:
 
-        if json_data['action'] == 'upload':
-            if not json_data['filename'].endswith('.py'):
-                return {'status': 'failed', 'message': 'Dit bestandstype wordt niet ondersteund. '}
-            file_data = base64.b64decode(json_data['file_data'])
-            with open('Patterns/' + json_data['filename'], 'wb') as f:
-                f.write(file_data)
-            return {'status': 'success'}
+		#Check pin
+		if json_data['pin'] != PIN:
+			return {'status': 'failed', 'message': 'Incorrect pin. '}
 
-        elif json_data['action'] == 'get_file':
-            try:
-                with open('Patterns/' + json_data['filename'], 'rb') as f:
-                    return {'status': 'success', 'file_data': base64.b64encode(f.read()).decode()}
-            except FileNotFoundError:
-                return {'status': 'failed', 'message': 'Dit bestand is niet meer beschikbaar. '}
+		# Upload file to library
+		if json_data['action'] == 'upload':
+			if not json_data['filename'].endswith('.py'):
+				return {'status': 'failed', 'message': 'Dit bestandstype wordt niet ondersteund. '}
+			file_data = base64.b64decode(json_data['file_data'])
+			with open('Patterns/' + json_data['filename'], 'wb') as f:
+				f.write(file_data)
+			return {'status': 'success'}
 
-        elif json_data['action'] == 'get_library':
-            return {'status': 'success', 'library': load_library()}
+		# Get file contents from library
+		elif json_data['action'] == 'get_file':
+			try:
+				with open('Patterns/' + json_data['filename'], 'rb') as f:
+					return {'status': 'success', 'file_data': base64.b64encode(f.read()).decode()}
+			except FileNotFoundError:
+				return {'status': 'failed', 'message': 'Dit bestand is niet meer beschikbaar. '}
 
-        elif json_data['action'] == 'get_queue':
-            return {'status': 'success', 'queue': ['kweewee.py', 'penis_dicks.py', 'dickiedick.py', 'sloppy_code.py']}
+		# Upload file to library
+		elif json_data['action'] == 'get_library':
+			return {'status': 'success', 'library': load_library()}
 
-        elif json_data['action'] == 'add_to_queue':
-            queue.append(json_data['filename']) #Filename is not checked yet
-            return {'status': 'success'}
+		# Get queue contents
+		elif json_data['action'] == 'get_queue':
+			return {'status': 'success', 'queue': ['kweewee.py', 'penis_dicks.py', 'dickiedick.py', 'sloppy_code.py']}
 
-        elif json_data['action'] == 'delete_from_queue':
-            queue.remove(json_data['filename'])
-            return {'status': 'success'}
+		# Send the updated queue content back to the server
+		elif json_data['action']== 'send_queue':
+			
+			return {'status': 'success'}
 
-        elif json_data['action'] == 'move_up':
-            index = queue.index(json_data['filename'])
-            queue[index], queue[index - 1] = queue[index - 1], queue[index]
-            return {'status': 'success'}
+		# Terminate the currently active pattern
+		elif json_data['action'] == 'kill_current':
+			d.stop = True
+			t = Thread(target = d.start)
+			t.daemon = True
+			t.start()
+			return {'status': 'success'}
 
-        elif json_data['action']== 'move_down':
-            index = queue.index(json_data['filename'])
-            queue[index], queue[index + 1] = queue[index + 1], queue[index]
-            return {'status': 'success'}
+		# Delete pattern from library
+		elif json_data['action'] == 'delete_from_library':
+			if not os.path.exists('Patterns/' + json_data['filename']):
+				return {'status': 'failed', 'message': 'Dit bestand bestaat niet (meer).  '}
+			move('Patterns/' + json_data['filename'], 'Wastebin/' + json_data['filename'])
+			return {'status': 'success'}
 
-        elif json_data['action'] == 'kill_current':
-            d.stop = True
-            t = Thread(target = d.start)
-            t.daemon = True
-            t.start()
-            return {'status': 'success'}
-
-        elif json_data['action'] == 'delete_from_library':
-            if not os.path.exists('Patterns/' + json_data['filename']):
-                return {'status': 'failed', 'message': 'Dit bestand bestaat niet (meer).  '}
-            move('Patterns/' + json_data['filename'], 'Wastebin/' + json_data['filename'])
-            return {'status': 'success'}
-
-    except KeyError:
-        pass
-    return {'status': 'failed', 'message': 'Something went wrong. '}
+	except KeyError:
+		pass
+	return {'status': 'failed', 'message': 'Something went wrong. '}
 
 class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        file = self.path
-        if file == '/':
-            file = 'index.html'
-        try:
-            with open(file.split('/')[-1], 'rb') as f:
-                self.send_response(200)
-                self.send_header('Content-type', 'text/' + file.split('/')[-1].split('.')[-1])
-                self.end_headers()
-                self.wfile.write(f.read())
-        except FileNotFoundError:
-            self.send_response(404)
-            self.end_headers()
+	def do_GET(self):
+		file = self.path
+		if file == '/':
+			file = 'index.html'
+		try:
+			with open(file.split('/')[-1], 'rb') as f:
+				self.send_response(200)
+				self.send_header('Content-type', 'text/' + file.split('/')[-1].split('.')[-1])
+				self.end_headers()
+				self.wfile.write(f.read())
+		except FileNotFoundError:
+			self.send_response(404)
+			self.end_headers()
 
-    def do_POST(self):
-        data_string = self.rfile.read(int(self.headers['Content-Length']))
-        json_data = json.loads(data_string.decode())
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(handle_json(json_data)).encode())
+	def do_POST(self):
+		data_string = self.rfile.read(int(self.headers['Content-Length']))
+		json_data = json.loads(data_string.decode())
+		self.send_response(200)
+		self.send_header('Content-type', 'application/json')
+		self.end_headers()
+		self.wfile.write(json.dumps(handle_json(json_data)).encode())
 
-    def log_message(self, format, *args):
-        pass
+	def log_message(self, format, *args):
+		pass
 
 def main():
-    t = Thread(target = d.start)
-    t.daemon = True
-    t.start()
-    try:
-        server = HTTPServer((HOST, PORT), RequestHandler)
-        server.serve_forever()
-    except KeyboardInterrupt:
-        pass
+	t = Thread(target = d.start)
+	t.daemon = True
+	t.start()
+	try:
+		server = HTTPServer((HOST, PORT), RequestHandler)
+		server.serve_forever()
+	except KeyboardInterrupt:
+		pass
 
 if __name__ == '__main__':
-    main()
+	main()
